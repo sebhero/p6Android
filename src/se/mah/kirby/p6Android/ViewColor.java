0bd753a73177280d63c992e008d5c5969dbe159e
seb <trevligt.att.ses@gmail.com>
2016-01-06 21:49:39 +0100

    Revert "ViewColor fixad!!! kan kanske göras visuellt snyggare"
    This reverts commit c8cfc063a62c809a65bd8ced0e444865361b86f2.

--- src/se/mah/kirby/p6Android/ViewColor.java
+++ src/se/mah/kirby/p6Android/ViewColor.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import se.mah.kirby.Color.Color;
 import se.mah.kirby.Color.ColorDisplay;
 import se.mah.kirby.controller.Controller;
-import se.mah.kirby.model.Array7;
 import se.mah.kirby.model.Array7x7;
 import se.mah.kirby.view.ViewImpl;
 
@@ -32,7 +31,7 @@ public class ViewColor extends Activity implements ViewImpl{
 
         colorDisplay =  (ColorDisplay) findViewById(R.id.colorDisplay);
         colorDisplay.setBackgroundColor(Color.BLACK);
-        colorDisplay.setGridColor(Color.PINK);
+        colorDisplay.setGridColor(Color.YELLOW);
 
         //Get Global Controller Class object (see application tag in AndroidManifest.xml)
         ctrl= (Controller) getApplicationContext();
@@ -81,98 +80,11 @@ public class ViewColor extends Activity implements ViewImpl{
             this.color = Color.BLACK;
     }
 
-//    public void randColor(View v){
-  //      ctrl.showRandomColor();
-    //}
-
-    Array7 arr;
-    public void chooseColor(View v){
-        switch (v.getId()){
-            case R.id.btn_row1:
-                updateColor(tick1);
-                ctrl.setElement(0, 0, color);
-                tick1++;
-                if(tick1 > 10)
-                    tick1 = 0;
-                break;
-            case R.id.btn_row2:
-                updateColor(tick2);
-                ctrl.setElement(1, 0, color);
-                tick2++;
-                if(tick2 > 10)
-                    tick2 = 0;
-                break;
-            case R.id.btn_row3:
-                updateColor(tick3);
-                ctrl.setElement(2, 0, color);
-                tick3++;
-                if(tick3 > 10)
-                    tick3 = 0;
-                break;
-            case R.id.btn_row4:
-                updateColor(tick4);
-                ctrl.setElement(3, 0, color);
-                tick4++;
-                if(tick4 > 10)
-                    tick4 = 0;
-                break;
-            case R.id.btn_row5:
-                updateColor(tick5);
-                ctrl.setElement(4, 0, color);
-                tick5++;
-                if(tick5 > 10)
-                    tick5 = 0;
-                break;
-            case R.id.btn_row6:
-                updateColor(tick6);
-                ctrl.setElement(5, 0, color);
-                tick6++;
-                if(tick6 > 10)
-                    tick6 = 0;
-                break;
-            case R.id.btn_row7:
-                updateColor(tick7);
-                ctrl.setElement(6, 0, color);
-                tick7++;
-                if(tick7 > 10)
-                    tick7 = 0;
-                break;
-            case R.id.btn_shiftUp:
-                ctrl.setDirection(Controller.DIRECTION.UP);
-                arr = new Array7();
-                ctrl.showShift(arr);
-                break;
-            case R.id.btn_shiftDown:
-                ctrl.setDirection(Controller.DIRECTION.DOWN);
-                arr = new Array7();
-                ctrl.showShift(arr);
-                break;
-            case R.id.btn_shiftLeft:
-                resetTick();
-                ctrl.setDirection(Controller.DIRECTION.LEFT);
-                arr = new Array7();
-                ctrl.showShift(arr);
-                break;
-            case R.id.btn_shiftRight:
-                resetTick();
-                ctrl.setDirection(Controller.DIRECTION.RIGHT);
-                arr = new Array7();
-                ctrl.showShift(arr);
-                break;
-            case R.id.btn_randColor:
-                ctrl.showRandomColor();
-                break;
-            case R.id.btn_oneColor:
-                resetTick();
-                updateColor(tickBig);
-                ctrl.showSameColor(color);
-
-                tickBig++;
-                if(tickBig > 9)
-                    tickBig = 0;
-                break;
-        }
+    public void randColor(View v){
+        System.out.println("hello");
+        ctrl.showRandomColor();
     }
+
 
     @Override
     public void setCtrl(Controller ctrl) {
